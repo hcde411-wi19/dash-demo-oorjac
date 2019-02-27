48623dcd7d748505c8955e3075d692b0d306d1ba
Oorja Chowdhary <oorjac@uw.edu>
2019-02-26 20:52:23 -0800

    finished coding for e1 and 2

--- exercise2.py
+++ exercise2.py
@@ -2,8 +2,55 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly.graph_objs as go
+import pandas as pd
 
+# initialize Dash app and initialize the static folder
+app = dash.Dash(__name__, static_folder='static')
+df = pd.read_csv('static/Pokemon.csv')
+# set layout of the page
+app.layout = html.Div(children=[
 
-# Exercise 2
-# Create another visualization of your choice of data and chart type.
-# You can use pandas to help loading data, or just hard-coded the data is fine.
+    # set the page heading
+    html.H1(children='L2 Exercise 2'),
+
+    # set the description underneath the heading
+    html.Div(children='''
+        This scatter plot was created to see whether Pokemon's attack points 
+        effected the amount of defense points they could gain and vice versa.
+    '''),
+
+    # append the visualization to the page
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            # configure the data
+            'data': [
+                # This is how we define a scatter plot. Note that it also uses "go.Scatter",
+                # but with the mode to be only "markers"
+                go.Scatter(
+                    x=df['Attack'],
+                    y=df['Defense'],
+                    mode='markers',
+                    text=df['Name'],# This line sets the vehicle name as the points' labels.
+                    marker={
+                        'size': 8,
+                        'opacity': .6,  # By making the points a bit transparent, it can alleviate the occlusion issue
+                        'color': 'green'
+                    }
+                )
+            ],
+            'layout': {
+                'title': 'Pokemon Attack Points vs. Defense Points',
+                # It is always a good practice to have axis labels.
+                # This is especially important in this case as the numbers are not trivial
+                'xaxis': {'title': 'Attack Points'},
+                'yaxis': {'title': 'Defense Points'},
+            }
+        }
+    )
+
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
